373e46e03a98ce52ea7693ec15a3b474ce06e321
theordersoulfly <65884146+theordersoulfly@users.noreply.github.com>
2020-06-08 02:17:53 +0300

    Update and rename requirement.txt to requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,15 @@
+#!/bin/sh
+#color
+
+cd $dir
 pip install bs4
+clear
 pip install colorama 
+clear
 pip install requests
+clear
 pip install worldlist
+clear
 pip install metasploit
+clear
+echo " Hacked By Huso" 
