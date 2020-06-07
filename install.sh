1b9fd4733e71e5373908fcac45b2ca9c5b7d3fa1
theordersoulfly <65884146+theordersoulfly@users.noreply.github.com>
2020-06-08 02:13:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ dir=/data/data/com.termux/files/home
 server=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs
 
 cd $dir
-chmod +700 EasY_HaCk/*
 clear
 apt-get update -y
 clear
@@ -13,15 +12,15 @@ apt upgrade -y
 clear
 pkg install figlet -y
 clear
-figlet -f small installation
+figlet -f small indiriliyor 
 sleep 1
-figlet -f small '0f'
+figlet -f small "By"
 sleep 1
-figlet -f small EasY HaCk
+figlet -f small "Huso"
 sleep 1
-figlet -f small Framwork
+figlet -f small ":)" 
 sleep 1
-echo $W "Installing requirements........"$G
+echo " Paketler yukleniyor......." 
 apt-get install curl -y
 apt-get install tor -y
 apt-get install perl -y
@@ -38,71 +37,21 @@ echo "Pleas accept........"
 sleep 3
 termux-setup-storage
 sleep 6
-mkdir /sdcard/EasY_HaCk
+mkdir /sdcard/Huso
 cd $dir
-mkdir EasY_HaCk-results
+mkdir Huso-results
 cd $dir
-pip install -r EasY_HaCk/.modules/.Infoga/requirements.txt
-clear
-sleep 1
-pip2 install -r EasY_HaCk/.modules/.recon-ng/REQUIREMENTS
-clear
-sleep 1
 pip install PySocks
 clear
+pip install bs4
 sleep 1
-python3 EasY_HaCk/.modules/.slowloris/setup.py build
-clear
-python3 EasY_HaCk/.modules/.slowloris/setup.py install
-clear
-clear
-echo " Downloding start up .................."
+echo " Indirme baslatiliyor.................."
 sleep 1
-echo " " $G
+echo " pip dosyaları yükleniyor" $G
 pip install --upgrade pip
 pip install wordlist
-mkdir $PREFIX/share/apache2/default-site/htdocs/zaki
-cd $dir
-rm -rf $server/index.html
-mv EasY_HaCk/.modules/Algeria.gif $server/
-mv EasY_HaCk/.modules/index.html $server/
-rm -rf EasY_HaCk/.modules/index.html 
-rm -rf EasY_HaCk/.modules/Algeria.gif
-cat EasY_HaCk/.modules/zaki.tar.gza* > EasY_HaCk/zaki.tar  
-tar -zxvf EasY_HaCk/zaki.tar
-mv zaki.txt EasY_HaCk/.modules/
-cd $dir
-gcc EasY_HaCk/.modules/.xerxes/xerxes.c -o xerxes 
-mv EasY_HaCk/xerxes EasY_HaCk/.modules/.xerxes/
-chmod +x EasY_HaCk/*
-chmod +x EasY_HaCk/.modules/.*
-chmod +x EasY_HaCk/.modules/*
-chmod +x EasY_HaCk/.modules/.Infoga/*
-chmod +x EasY_HaCk/.modules/.theHarvester/*
-chmod +x EasY_HaCk/.modules/.CMSeeK/*
-chmod +x EasY_HaCk/.modules/.RED_HAWK/*
-chmod +x EasY_HaCk/.modules/.metagoofil/*
-chmod +x EasY_HaCk/.modules/.recon-ng/*
-chmod +x EasY_HaCk/.modules/.Python-Hash-Cracker/*
-chmod +x EasY_HaCk/.modules/.torshammer/*
-chmod +x EasY_HaCk/.modules/.slowloris/*
-chmod +x EasY_HaCk/.modules/.xerxes/*
-chmod +x EasY_HaCk/.modules/.sqlmap/*
-chmod +x EasY_HaCk/.modules/.theHarvester/*
-chmod +x EasY_HaCk/.modules/.metagoofil/*
-chmod +x EasY_HaCk/.modules/.Hash*
-cd $dir
-rm -rf EasY_HaCk/zaki.tar
-rm -rf EasY_HaCk/zaki.tar.gza*
-rm -rf EasY_HaCk/.modules/zaki.tar.gza*
-mv -f EasY_HaCk/zaki.txt EasY_HaCk/.modules/
-mv -f EasY_HaCk/EasY_HaCk $PREFIX/bin/
-mv -f EasY_HaCk $PREFIX/share/
-cd $dir
-rm -rf $HOME/EasY_HaCk
 sleep 3
 clear
-figlet -f small "   DONE!"
-echo "Now Type in new terminal ————>  EasY_HaCk"
+figlet -f big " Hacked By Huso"
+echo " JustWatchHackTeam" 
 sleep 3
-EasY_HaCk 
